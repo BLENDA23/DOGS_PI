fa4ea35272808c071133a66798546df351b2f805
BLENDA23 <blenda.prg@gmail.com>
2023-05-10 17:40:09 -0500

    front

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
-
+import Bienvenida from "./components/bienvenida/Bienvenida";
+import HomePage from "./components/homePage/HomePage";
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <h1>Henry Dogs</h1>
+      <Routes>
+        <Route path="/" element={<Bienvenida />}></Route>
+        <Route path="/homePage" element={<HomePage />}></Route>
+      </Routes>
     </div>
   );
 }
