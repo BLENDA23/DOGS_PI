024212ddb1b861458728a3e0bc66f0d998a7397f
BLENDA23 <blenda.prg@gmail.com>
2023-05-10 18:09:53 -0500

    front

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import Bienvenida from "./components/bienvenida/Bienvenida";
 import HomePage from "./components/homePage/HomePage";
-import { Routes, Route } from "react-router-dom";
+import Nav from "./components/nav/Nav";
+import About from "./components/homePage/HomePage";
+import { Routes, Route,useLocation, useNavigate  } from "react-router-dom";
 function App() {
+  const navigate = useNavigate();
+  const location = useLocation();
   return (
     <div className="App">
-      <h1>Henry Dogs</h1>
+      <h1>Henry Dogsx</h1>
+      {location.pathname !== "/" && <Nav/>}
       <Routes>
-        <Route path="/" element={<Bienvenida />}></Route>
-        <Route path="/homePage" element={<HomePage />}></Route>
+        <Route path="/" element={<Bienvenida/>}></Route>
+        <Route path="/homePage" element={<HomePage/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
       </Routes>
     </div>
   );
---
+++ client/src/components/nav/Nav.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import styles from "./Nav.module.css"
+import { NavLink } from "react-router-dom";
+export default function HomePage(props) {
+    return (
+      <div>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/favorites">Favorites</NavLink>
+      </div>
+    );
+}
