214495c4b62d894e01d1e1097aa77c24f71bc40e
BLENDA23 <blenda.prg@gmail.com>
2023-05-18 21:29:38 -0500

    18-05-2023-2129

--- client/src/components/cards/Cards.jsx
+++ client/src/components/cards/Cards.jsx
@@ -1,11 +1,29 @@
 import Card from "../card/Card";
 import styles from "./Cards.module.css";
+import { useEffect, useState } from "react";
 export default function Cards(props) {
   const { dogs } = props;
   console.log(dogs);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 8; // Cantidad de elementos por página
+  const totalItems = 100; // Cantidad total de elementos en la lista
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+  // Función para cambiar de página
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  // Cálculo del rango de índices de los elementos a mostrar
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  // Filtrado de elementos según el rango de índices
+  const paginatedItems = dogs.slice(startIndex, endIndex);
+
   return (
     <div>
-      {dogs.map(({ id, name, bred_for, breed_group, life_span,image,weight,temperament }) => (
+      {paginatedItems.map(({ id, name, bred_for, breed_group, life_span,image,weight,temperament }) => (
         <Card
           id={id}
           key={id}
@@ -18,6 +36,21 @@ export default function Cards(props) {
           temperament={temperament}
         />
       ))}
+    
+      <div>
+        {/* Botones de paginación */}
+        {currentPage > 1 && (
+          <button onClick={() => handlePageChange(currentPage - 1)}>
+            Anterior
+          </button>
+        )}
+
+        {currentPage < totalPages && (
+          <button onClick={() => handlePageChange(currentPage + 1)}>
+            Siguiente
+          </button>
+        )}
+      </div>
     </div>
   );
 }
